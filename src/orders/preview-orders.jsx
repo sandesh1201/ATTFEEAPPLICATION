e35602d1f80f3338a30170af6faf07baa5e294ea
sandesh1201 <38392443+sandesh1201@users.noreply.github.com>
2021-03-03 10:49:34 -0600

    hamburger fix

--- src/orders/preview-orders.jsx
+++ src/orders/preview-orders.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './preview-orders.css';
 import iphone from '../images/iphoneXR.png';
 import { setCurrentOrderId, setOrders } from '../actions';
 const axios = require('axios');
@@ -26,7 +27,8 @@ class PreviewOrdersComponent extends React.Component {
             this.props.orders && this.props.orders.map((order, index) => {
             return (
             <Link to={`/order/${order.orderId}`} key={index}>
-            <div className="card d-none d-lg-block d-md-block " key={index} style={{width:'12rem'}} onClick={() =>this.getOrderDetails(order.orderId)}>
+            <div>
+            <div className="card d-none d-md-block d-lg-block "  key={index}  onClick={() =>this.getOrderDetails(order.orderId)}>
               <div className="row no-gutters">
                   <div className="col-sm-5 col-md-6 col-lg-5" >
                       <img src={iphone} className="img-fluid" />
@@ -53,6 +55,64 @@ class PreviewOrdersComponent extends React.Component {
                       </div>
                   </div>
               </div>
+            </div>
+            </div>
+           <div>
+            <div className=" card d-md-none d-lg-none d-sm-block   " key={index}  onClick={() =>this.getOrderDetails(order.orderId)}>
+              <div className="row no-gutters">
+                  <div className="col-sm-2 col-md-6 col-lg-5" >
+                      <img src={iphone} className="img-fluid" />
+                  </div>
+                  <div className="col-sm-10 col-md-6 col-lg-7">
+                      <div className="card-block px-2">
+                        
+                          <div  className="menu-items">
+                          <div className="row order-detail-not-shipped" >
+                    <div className="status-tracker">
+                        <div>
+                            <i className="fa fa-shopping-cart fa-lg  center"></i>
+                        </div>
+                        <div className="line"></div>
+                        <div >
+                            Ordered
+                        </div>
+                    </div>
+
+                    <div className="status-tracker" >
+                        <div>
+                            <i className="fa fa-truck  fa-lg center "></i>
+                        </div>
+                        <div className="line"></div>
+                        <div >
+                            Shipped
+                    </div>
+                    </div>
+                    <div className="status-tracker">
+                        <div>
+                            <i className="fa fa-home fa-lg center "></i>
+                        </div>
+                        <div className="line"></div>
+                        <div >
+                            Delivered
+                        </div>
+                    </div>
+                </div>
+                          </div>
+                          {order.items.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <p className="main-font-size">{item.name}</p>
+                                        {/* <p className="side-font">Color: {item.skuAttributes.color}</p>
+                                        <p className="card-text">Size: {item.skuAttributes.size}</p> */}
+                                        <p className="side-font">Quantity: {item.quantity}</p>
+                                    </div>
+                                )
+                                })
+                            }
+                      </div>
+                  </div>
+              </div>
+            </div>
             </div>
             </Link>
                 )
